548ec1a9b19bf8a1a2b0007745fd834412922851
Haroldo <haroldovinente@gmail.com>
2024-01-01 13:38:16 -0300

    mudando response para html

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,11 @@ import log
 
 def lambda_handler(event, context):
     log.log(f"event: {event}")
-    return {"statusCode": 200, "body": json.dumps(event)}
+    # return {"statusCode": 200, "body": json.dumps(event)}
+    return {
+        "statusCode": 200,
+        "body": f"<html><body>Dados da requisicao {json.dumps(event)}</body></html>",
+    }
 
 
 lambda_handler("aauau", "")
